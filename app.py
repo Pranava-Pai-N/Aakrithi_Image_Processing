a5553c9a35f36ac08212e81ea2ff01fa67599f68
Pranav Pai N <pranavpai0309@gmail.com>
2025-04-09 23:57:18 +0530

    Added a image processing feature using Contrastive Language Image Pretraining(CLIP)

--- app.py
+++ app.py
@@ -1,39 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
-import fitz  
-import cv2
-import pytesseract
+from ModelCode import classify_image
+from PIL import Image
 import os
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the PDF and Image to Text Extraction API!"}
-
-@app.post("/extract_text_from_pdf")
-async def extract_text_from_pdf(file: UploadFile = File(...)):
-    try:
-        file_path = f"./{file.filename}"
-        if not(file.filename.endswith('.pdf')): 
-            return {"error": "File is not a PDF"}
-        
-        with open(file_path, "wb") as pdf_file:
-            pdf_file.write(await file.read())
-
-        doc = fitz.open(file_path)
-        text = ""
-        for page in doc:
-            text += page.get_text()
-
-        return {"extracted_text": text}
-    except Exception as e:
-        return {"error": str(e)}
-    finally:
-        if os.path.exists(file_path):
-            os.remove(file_path)
+    return {"message": "Welcome to Image Processing and Classification API!"}
 
 
-@app.post("/extract_text_from_image")
+@app.post("/Image_Processing")
 async def extract_text_from_image(file: UploadFile = File(...)):
     try:
         file_path = f"./{file.filename}"
@@ -42,12 +19,15 @@ async def extract_text_from_image(file: UploadFile = File(...)):
         
         with open(file_path, "wb") as img_file:
             img_file.write(await file.read())
-
-        img = cv2.imread(file_path)
-        text = pytesseract.image_to_string(img)
-        return {"extracted_text": text}
+            
+        result = classify_image(file_path)
+        return {
+            "Type":str(result["Type"]),
+        }
+         
     except Exception as e:
         return {"error": str(e)}
+    
     finally:
         if os.path.exists(file_path):
             os.remove(file_path)
